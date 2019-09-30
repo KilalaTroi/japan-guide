792c817ef7c4d454326fef551fad1ed4052e62f3
KILALA_Troi <troi.hoang@kilala.vn>
2019-09-30 17:16:14 +0700

    remove output out of git

---
+++ wp-content/themes/japanguide/404.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+<div class="container py-5">
+    <div class="row">
+        <div class="col-sm-8">
+            <h1>404 Error!</h1>
+            <div class="content">
+                <p>Page not found!</p>
+            </div>
+        </div>
+    </div>
+</div>
+<?php get_footer(); ?>
