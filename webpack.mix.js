f1478262fe3224a6631f0a004cc807d4c349743e
KILALA_Troi <troi.hoang@kilala.vn>
2019-10-01 15:31:10 +0700

    update frontend task run

--- webpack.mix.js
+++ webpack.mix.js
@@ -18,8 +18,6 @@ const ssi = require('browsersync-ssi');
 const public_path = 'wp-content/themes/japanguide/';
 const html_path = 'html/wp-content/themes/japanguide/';
 
-console.log(mix.config.publicPath);
-
 // resourceRoot
 mix.config.resourceRoot = 'src';
 
@@ -32,31 +30,51 @@ mix.webpackConfig({
             {
                 test: /\.scss$/,
                 loader: 'import-glob-loader'
-            },
+            }
         ]
     }
 });
 
+mix.options({ 
+    processCssUrls: false
+});
+
 if ( mix.config.production ) {
+
     // production
-    mix.js(mix.config.resourceRoot + '/assets/js/app.js', public_path + 'assets/js')
-        .js(mix.config.resourceRoot + '/assets/js/landing.js', public_path + 'assets/js')
-        .sass(mix.config.resourceRoot + '/assets/sass/app.scss', public_path + 'assets/css')
-        .sass(mix.config.resourceRoot + '/assets/sass/landing.scss', public_path + 'assets/css');
+    mix.js(mix.config.resourceRoot + '/assets/js/app.js', public_path + 'assets/js');
+    // mix.js(mix.config.resourceRoot + '/assets/js/landing.js', public_path + 'assets/js');
+    mix.sass(mix.config.resourceRoot + '/assets/sass/app.scss', public_path + 'assets/css');
+    // mix.sass(mix.config.resourceRoot + '/assets/sass/landing.scss', public_path + 'assets/css');
 
 } else {
     // development
     mix.config.sourcemaps = true;
 
-    mix.js(mix.config.resourceRoot + '/assets/js/app.js', public_path + 'assets/js')
-        .js(mix.config.resourceRoot + '/assets/js/landing.js', public_path + 'assets/js')
-        .sass(mix.config.resourceRoot + '/assets/sass/app.scss', public_path + 'assets/css')
-        .sass(mix.config.resourceRoot + '/assets/sass/landing.scss', public_path + 'assets/css').sourceMaps();
+    // ------------------- JS ----------------------//
+    mix.js(mix.config.resourceRoot + '/assets/js/app.js', public_path + 'assets/js');
+    // mix.js(mix.config.resourceRoot + '/assets/js/landing.js', public_path + 'assets/js');
+    // mix.extract(['vue']);
+    // ------------------- End JS ----------------------//
 
+    // ------------------- CSS ---------------------//
+    mix.webpackConfig({devtool: 'inline-source-map'});
+        
+    mix.sass(mix.config.resourceRoot + '/assets/sass/app.scss', public_path + 'assets/css', {
+        implementation: require('node-sass'),
+        outputStyle: 'expanded',
+        sourceMap: true,
+    });
+    // mix.sass(mix.config.resourceRoot + '/assets/sass/landing.scss', public_path + 'assets/css');
+    // ------------------- End CSS ---------------------//
+    
+    // ------------------- Coppy -----------------------//
     mix.copyDirectory(public_path + '/assets', html_path + '/assets');
-
+    // ------------------- End Coppy -----------------------//
+    
+    // ------------------- browserSync -----------------------//
     browserSync.init({
-        watch: true,
+        // watch: true,
         server: {
             baseDir: ['html'],
             middleware: ssi({
@@ -65,6 +83,6 @@ if ( mix.config.production ) {
             })
         },
     });
-
-    console.log(__dirname);
 }
+
+mix.disableNotifications();
